781614fd9612394aa2698f2d46c93f2df52c7249
djamelkorei <korei.djamel.eddine@gmail.com>
2023-09-26 23:16:53 +0100

    update the create document

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { CreateDocumentDto } from './dto/create-document.dto';
-import { UpdateDocumentDto } from './dto/update-document.dto';
+import {Injectable} from '@nestjs/common';
+import {CreateDocumentDto} from './dto/create-document.dto';
+import {UpdateDocumentDto} from './dto/update-document.dto';
 import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
+import {DataSource, Repository} from 'typeorm';
 import {Document} from "./entities/document.entity";
+import {embeddingText} from "../utils/search";
 
 @Injectable()
 export class DocumentsService {
 
-  constructor(@InjectRepository(Document) private documentRepository: Repository<Document>) {}
+  constructor(@InjectRepository(Document) private documentRepository: Repository<Document>,
+              private dataSource: DataSource) {}
 
   async create(createDocumentDto: CreateDocumentDto) : Promise<Document> {
     console.log('This action adds a new document');
-    const document : Document = {...createDocumentDto, id : null, embedding: []}
-    return this.documentRepository.create(document);
+    const embedding = await embeddingText(createDocumentDto.content);
+    return await this.dataSource.manager.query(`insert into 
+        documents(company, content, embedding) 
+        values ('${createDocumentDto.company}',
+                '${createDocumentDto.content}',
+                '[${embedding.toString()}]'
+                )`);
   }
 
-  findAll() : Promise<Document[]> {
+   findAll() : Promise<Document[]> {
     console.log(`This action returns all documents`);
     return this.documentRepository.find();
   }
@@ -24,18 +31,19 @@ export class DocumentsService {
   findOne(id: number): Promise<Document | null> {
     console.log(`This action returns a #${id} document`);
     return this.documentRepository.findOneBy({id});
-
   }
 
   async update(id: number, updateDocumentDto: UpdateDocumentDto) : Promise<Document> {
     console.log(`This action updates a #${id} document`);
     let document = await this.documentRepository.findOneBy({id});
     document = {...document, ...updateDocumentDto};
-    return this.documentRepository.create(document);
+    await this.documentRepository.update(id, document);
+    return this.documentRepository.findOneBy({ id })
   }
 
   async remove(id: number) {
     console.log(`This action removes a #${id} document`);
     await this.documentRepository.delete(id);
   }
+
 }
